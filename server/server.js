c32b1eedb479d184186817ff847c07ab5f8faf69
benwato <benwato12@gmail.com>
2021-09-28 17:49:28 +0100

    Updates to routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 server.use(express.json());
-//
 
+const blogRoutes = require('./routes/blog')
+server.use('./blogs', blogRoutes)
 
 server.get('/', (req, res) => res.send('Welcome! This is Telegraph by Ben and Elena'))
 
